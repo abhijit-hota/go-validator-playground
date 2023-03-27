bf93f01b1230f2d2276da1de5e57ca6557e1228a
abhijit-hota <abhihota025@gmail.com>
2023-03-28 00:33:09 +0530

    🖼 frontend: initial render/cleanup

--- frontend/index.js
+++ frontend/index.js
@@ -72,3 +72,10 @@ const render = () => {
 			break;
 	}
 };
+
+// Instantiate WASM and first render
+const go = new Go();
+WebAssembly.instantiateStreaming(fetch('main.wasm'), go.importObject).then((result) => {
+	go.run(result.instance);
+	render();
+});
--- frontend/wasm_init.js
+++
@@ -1,4 +0,0 @@
-const go = new Go();
-WebAssembly.instantiateStreaming(fetch('main.wasm'), go.importObject).then((result) => {
-	go.run(result.instance);
-});
